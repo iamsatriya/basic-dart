1770b4afa3506f486ad17619ee6f9d1df31182e2
unknown <satriya.wicaksana888@gmail.com>
2020-09-27 21:25:45 +0700

    operator on dart

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,1 +1,21 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  /// assignment operator
+  var a = 10;
+
+  /// operator aritmatika
+  /// Fitur baru tau:
+  ///   divide hasil double => /
+  ///   divide hasil integer => ~/
+  print(5 + 2); // int add = 7
+  print(5 - 2); // int subtract = 3
+  print(5 * 2); // int multiply = 10
+  print(5 / 2); // double divide = 2.5
+  print(5 ~/ 2); // int intDivide = 2
+  print(5 % 2); // int modulo = 1
+
+  /// Operator perbandingan
+  /// sama kayak lain
+
+  /// Operator logika
+  /// sama kayak lain
+}
