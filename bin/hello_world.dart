627601ab30f1d0249592e70feba903fdb3717aa4
unknown <satriya.wicaksana888@gmail.com>
2020-09-28 21:10:11 +0700

    implement mixin

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,1 +1,28 @@
-void main(List<String> arguments) {}
+abstract class Performer {
+  void perform();
+}
+
+mixin Dancer implements Performer {
+  @override
+  void perform() {
+    print('Dancing');
+  }
+}
+
+mixin Singer implements Performer {
+  @override
+  void perform() {
+    print('Singing');
+  }
+}
+
+class Musician extends Performer with Singer, Dancer {
+  void showTime() {
+    perform();
+  }
+}
+
+void main(List<String> arguments) {
+  var ariel = Musician();
+  ariel.perform();
+}
