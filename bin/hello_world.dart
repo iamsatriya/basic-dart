7dbca86412336f71b9cc69bd8cf12bc20bd2d752
unknown <satriya.wicaksana888@gmail.com>
2020-09-28 19:25:41 +0700

    enum

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,1 +1,21 @@
-void main(List<String> arguments) {}
+enum StatusRequest { Todo, InProgress, InReview, Revision, Done }
+void main(List<String> arguments) {
+  /**
+   * enum bisa menyimpan nilai konstan, mengumpulkannya sehingga jadi mudah
+   */
+  print(StatusRequest.Todo);
+  print(StatusRequest.values);
+  print(StatusRequest.InProgress.index);
+
+  /// Dapat diimplement pada switch, tapi harus semua diimplement
+  /// tapi ternyata gak diimplement gak keluar error
+  var Task1 = StatusRequest.InReview;
+  switch (Task1) {
+    case StatusRequest.Todo:
+      print('Todo');
+      break;
+    case StatusRequest.InProgress:
+      print('In progress');
+      break;
+  }
+}
