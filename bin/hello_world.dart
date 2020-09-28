d61f1bc02e6f5e6f88705866ebd9308f93986b4b
unknown <satriya.wicaksana888@gmail.com>
2020-09-28 12:55:46 +0700

    collection type 3: map

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,1 +1,13 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  var map = {'name': 'satriya', 'age': 21, 'isActive': true};
+  print(map['name']);
+
+  // get map keys
+  print(map.keys);
+
+  // get map values
+  print(map.values);
+
+  // adding new pair key:value
+  map['newKey'] = 'newValue';
+}
