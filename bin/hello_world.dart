302aefe0f03ae6ae7e85522865663bd5a47caad7
unknown <satriya.wicaksana888@gmail.com>
2020-09-28 21:13:47 +0700

    implement extension in single file

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,1 +1,28 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  var list = [1, 4, 5, 6, 2, 7];
+  print(list);
+  var sortedList = list.sortAsc();
+  print(sortedList);
+}
+
+extension Sorting on List<int> {
+  List<int> sortAsc() {
+    var list = this;
+    var length = this.length;
+
+    for (int i = 0; i < length - 1; i++) {
+      int min = i;
+      for (int j = i + 1; j < length; j++) {
+        if (list[j] < list[min]) {
+          min = j;
+        }
+      }
+
+      int tmp = list[min];
+      list[min] = list[i];
+      list[i] = tmp;
+    }
+
+    return list;
+  }
+}
