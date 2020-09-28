fe586acbdad505022d60f0917106e28a59884db2
unknown <satriya.wicaksana888@gmail.com>
2020-09-28 10:29:59 +0700

    exceptions basic

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,1 +1,20 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  /// contoh salah satu exception
+  /// IntegerDivisionByZeroException
+  /// ada yg lain seperti:
+  ///   IOException, FormatException, TimeoutException
+  ///   CertificateException, DefferedLoadException, DOMException
+  ///   HttpException, HandshakeException etc.
+  try {
+    var a = 7;
+    var b = 0;
+    print(a ~/ b);
+  } on IntegerDivisionByZeroException {
+    print('Can\'t divide by zero');
+  } catch (e, s) {
+    print('Exception happended: $e');
+    print('Stacktrace: $s');
+  } finally {
+    print('This finally always executed');
+  }
+}
